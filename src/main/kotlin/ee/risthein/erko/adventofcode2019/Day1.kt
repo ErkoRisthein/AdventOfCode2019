585eb48235e0235d8d004fa31a919aed2420ad0a
Erko Risthein <erko@risthein.ee>
2019-12-08 17:36:11 +0200

    Day 1 part 2 solution

--- src/main/kotlin/ee/risthein/erko/adventofcode2019/Day1.kt
+++ src/main/kotlin/ee/risthein/erko/adventofcode2019/Day1.kt
@@ -1,13 +1,28 @@
 package ee.risthein.erko.adventofcode2019
 
+import kotlin.math.max
+
 class Day1 {
 
     fun getFuelRequired(mass: Int): Int {
-        return mass / 3 - 2
+        return max(mass / 3 - 2, 0)
     }
 
     fun getTotalFuelRequired(masses: List<Int>): Int {
         return masses.fold(0) { sum, mass -> sum + getFuelRequired(mass) }
     }
 
+    fun getAbsoluteFuelRequired(masses: List<Int>): Int {
+        return masses.fold(0) { sum, mass -> sum + getAbsoluteFuelRequired(mass) }
+    }
+
+    fun getAbsoluteFuelRequired(fuel: Int): Int {
+        return getAbsoluteFuelRequired(0, fuel)
+    }
+
+    private tailrec fun getAbsoluteFuelRequired(total: Int, fuel: Int): Int {
+        if (fuel <= 0) return total
+        val extraFuel = getFuelRequired(fuel)
+        return getAbsoluteFuelRequired(total + extraFuel, extraFuel)
+    }
 }
--- src/test/kotlin/ee/risthein/erko/adventofcode2019/Day1Test.kt
+++ src/test/kotlin/ee/risthein/erko/adventofcode2019/Day1Test.kt
@@ -19,11 +19,28 @@ class Day1Test {
     }
 
     @Test
-    fun `can calculate total fuel required`() {
+    fun `can calculate the total fuel required`() {
         val masses = File(javaClass.getResource("/day1input.txt").toURI()).readLines().map { it.toInt() }
 
         val totalFuelRequired = day1.getTotalFuelRequired(masses)
 
         assertThat(totalFuelRequired).isEqualTo(3358992)
+    }
+
+    @ParameterizedTest
+    @CsvSource("14, 2", "1969, 966", "100756, 50346")
+    fun `can calculate the absolute fuel required`(mass: Int, fuel: Int) {
+        val fuelRequired = day1.getAbsoluteFuelRequired(mass)
+
+        assertThat(fuelRequired).isEqualTo(fuel)
+    }
+
+    @Test
+    fun `can calculate the absolute fuel required`() {
+        val masses = File(javaClass.getResource("/day1input.txt").toURI()).readLines().map { it.toInt() }
+
+        val absoluteFuelRequired = day1.getAbsoluteFuelRequired(masses)
+
+        assertThat(absoluteFuelRequired).isEqualTo(5035632)
     }
 }
